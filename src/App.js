9d5170b8b5f85a1a6583bb8a657d665b0e61958d
Colin Reilly <johndoe@example.com>
2020-03-08 08:52:00 -0400

    Pokemon show in list.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { fetchPokemon } from './data';
 
-function App() {
-    fetchPokemon();
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <div className="App">
-            <h1>hello</h1>
-        </div>
-    );
+        this.state = {
+            pokemon: [],
+        }
+    }
+
+    componentDidMount() {
+        fetchPokemon().then((data) => {
+            this.setState({
+                pokemon: data,
+            })
+        });
+    }
+
+    render() {
+        const { pokemon } = this.state;
+
+        const pokemonElements = pokemon.map((x) => (<li>{x.num} {x.name} {x.type} {x.weaknesses}</li>))
+
+        return (
+            <div className="App"> 
+                <ul>
+                    {pokemonElements}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/data/data.js
+++ src/data/data.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const pokemonDataURL = 'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json';
 
-export default async function fetchPokemon() {
-    return await axios.get(pokemonDataURL).then((res) => res.data);
+export default function fetchPokemon() {
+    return axios.get(pokemonDataURL).then((res) => res.data.pokemon);
 }
